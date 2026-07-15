7f272994113fc84ffbc3c0ead9303d146788bf31
sanjushree24 <sanjumithra919@gmail.com>
2026-07-15 14:06:31 +0530

    first push

---
+++ Evennumberdigits.java
@@ -0,0 +1,13 @@
+class Evennumberdigits {
+    public int findNumbers(int[] nums) {
+        int count=0;
+        for (int num : nums){
+            String s= String.valueOf(num);
+            if(s.length() % 2 == 0){
+                count++;
+
+            }
+        }
+       return count; 
+    }
+}
