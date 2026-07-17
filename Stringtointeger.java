75a3ba039ec829278246641807d3a0f15e279f64
sanjushree24 <sanjumithra919@gmail.com>
2026-07-17 09:19:22 +0530

    Third push

---
+++ Stringtointeger.java
@@ -0,0 +1,33 @@
+public class Stringtointeger{
+    public int myAtoi(String s) {
+        int i = 0, sign = 1;
+        long num = 0;
+
+        // Skip leading spaces
+        while (i < s.length() && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // Check sign
+        if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        // Read digits
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            num = num * 10 + (s.charAt(i) - '0');
+
+            if (sign * num > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+            if (sign * num < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+
+            i++;
+        }
+
+        return (int)(sign * num);
+    }
+}
