3cc354e2fd39d26b5f27c307f55cb0c186e671cd
sanjushree24 <sanjumithra919@gmail.com>
2026-07-19 21:33:50 +0530

    fourth push

---
+++ DiningPhilosophers .java
@@ -0,0 +1,41 @@
+public class DiningPhilosophers {
+
+    private Semaphore[] forks = new Semaphore[5];
+    private Semaphore room = new Semaphore(4);
+
+    public DiningPhilosophers() {
+        for (int i = 0; i < 5; i++) {
+            forks[i] = new Semaphore(1);
+        }
+    }
+
+    public void wantsToEat(
+            int philosopher,
+            Runnable pickLeftFork,
+            Runnable pickRightFork,
+            Runnable eat,
+            Runnable putLeftFork,
+            Runnable putRightFork) throws InterruptedException {
+
+        int left = philosopher;
+        int right = (philosopher + 1) % 5;
+
+        room.acquire();
+
+        forks[left].acquire();
+        forks[right].acquire();
+
+        pickLeftFork.run();
+        pickRightFork.run();
+
+        eat.run();
+
+        putLeftFork.run();
+        putRightFork.run();
+
+        forks[right].release();
+        forks[left].release();
+
+        room.release();
+    }
+}
