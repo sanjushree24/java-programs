f9e21343771f25567ab65abde1ad85ef8d3f988c
sanju <sanjumithra919@gmail.com>
2026-07-22 10:00:02 +0530

    fifth push

---
+++ Detectcapital.java
@@ -0,0 +1,16 @@
+public class Detectcapital {
+    public boolean detectCapitalUse(String word) {
+        int capcount=0;
+        for(int i=0;i<word.length();i++){
+           if(Character.isUpperCase(word.charAt(i))){
+            capcount++;
+           }
+            
+        }
+        if (capcount==word.length())return true;
+        if (capcount==0)return true;
+        if (capcount==1 && Character.isUpperCase(word.charAt(0)))return true;
+        return false;
+   
+    }
+}
