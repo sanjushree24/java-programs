3248245ca573387acc81a1f974ab83a0df450893
sanju <sanjumithra919@gmail.com>
2026-07-22 22:50:37 +0530

    string methods

---
+++ Jewelsandstones .java
@@ -0,0 +1,15 @@
+public class Jewelsandstones {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            
+                if(jewels.contains(""+stones.charAt(i))){
+                    count++;
+                    
+                }
+            
+        }
+        return count;
+        
+    }
+}
